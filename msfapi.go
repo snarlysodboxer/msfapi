fbde9c659b54c9afd6b22c2f9928af75ad6f822e
david amick <github@davidamick.com>
2015-02-07 19:22:11 +0000

    Add more auth methods.

--- msfapi.go
+++ msfapi.go
@@ -27,22 +27,64 @@ func New(url string) *API {
 	return api
 }
 
-func (api *API) AuthLogin(user, pass string) error {
+func (api *API) AuthLogin(user, pass string) (string, error) {
 	var loginResponse map[string]interface{}
 	var login = []string{"auth.login", user, pass}
 	err := api.request(&login, &loginResponse)
 	if err != nil {
-		return err
+		return "", err
 	}
 	if loginResponse["error"] != nil {
 		if loginResponse["error"].(bool) {
-			return errors.New(fmt.Sprintf("%#v, %#v",
+			return "", errors.New(fmt.Sprintf("%#v, %#v",
 				loginResponse["error_class"].(string),
 				loginResponse["error_message"].(string),
 			))
 		}
 	}
-	api.Token = fmt.Sprintf("%v", loginResponse["token"])
+	token := fmt.Sprintf("%v", loginResponse["token"])
+	return token, nil
+}
+
+func (api *API) AuthTokenAdd(token string) error {
+	var response map[string]string
+	var request = []string{"auth.token_add", api.Token, token}
+	err := api.request(&request, &response)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (api *API) AuthTokenGenerate() (string, error) {
+	var response map[string]string
+	var request = []string{"auth.token_generate", api.Token}
+	err := api.request(&request, &response)
+	if err != nil {
+		return "", err
+	}
+	token := response["token"]
+	return token, nil
+}
+
+func (api *API) AuthTokenList() ([]string, error) {
+	var response map[string][]string
+	var request = []string{"auth.token_list", api.Token}
+	err := api.request(&request, &response)
+	if err != nil {
+		return []string{}, err
+	}
+	tokens := response["tokens"]
+	return tokens, nil
+}
+
+func (api *API) AuthTokenRemove(token string) error {
+	var response map[string]string
+	var request = []string{"auth.token_remove", api.Token, token}
+	err := api.request(&request, &response)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
